0e100a42dbfaea03f0981efcf8915d4095bc9b91
unknown <Jeremy@.(none)>
2011-07-06 06:10:54 +1000

    removed debug from ptions

--- project/build/project.scala
+++ project/build/project.scala
@@ -3,5 +3,5 @@ import sbt._
 class ScalaAppEngineProject(info: ProjectInfo) extends AppengineProject(info) with IdeaProject with JRebel {
   val liftTextile = "net.liftweb" %% "lift-textile" % "2.2"
 
-  override def jrebelJvmOptions = List("-Xdebug", "-Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=5005")
+//  override def jrebelJvmOptions = List("-Xdebug", "-Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=5005")
 }
